ad5bd3c324fd59dcbd6db8f65fbab5b4a178a1d4
Berk Canpolat <canpolatberk19@gmail.com>
2025-09-10 17:23:06 +0300

    The navigation bar layout and dropdown menus have been completed

---
+++ script/app.js
@@ -0,0 +1,57 @@
+const exploreMenu = document.querySelector(".explore-menu");
+const exploreMenuOpen = document.querySelector(".explore-open");
+
+const moreMenu = document.querySelector(".more-menu");
+const moreMenuOpen = document.querySelector(".more-open");
+
+let menuTimer;
+
+// Explore Menu
+const exploreMenuOpenFunction = () => {
+    clearTimeout(menuTimer);
+    exploreMenuOpen.classList.add("explore-menu-js");
+}
+
+const exploreMenuCloseFunction = () => {
+    menuTimer = setTimeout(() => {
+        exploreMenuOpen.classList.remove("explore-menu-js");
+    }, 50);
+}
+
+// More Menu
+const moreMenuOpenFunction = () => {
+    clearTimeout(menuTimer);
+    moreMenuOpen.classList.add("more-menu-js");
+}
+
+const moreMenuCloseFunction = () => {
+    menuTimer = setTimeout(() => {
+        moreMenuOpen.classList.remove("more-menu-js");
+    }, 50);
+}
+
+
+// Explore Menu
+exploreMenu.addEventListener("mouseenter", exploreMenuOpenFunction);
+exploreMenuOpen.addEventListener("mouseenter", exploreMenuOpenFunction);
+
+exploreMenu.addEventListener("mouseleave", exploreMenuCloseFunction);
+exploreMenuOpen.addEventListener("mouseleave", exploreMenuCloseFunction);
+
+
+// More Menu
+moreMenu.addEventListener("mouseenter", moreMenuOpenFunction);
+moreMenuOpen.addEventListener("mouseenter", moreMenuOpenFunction);
+
+moreMenu.addEventListener("mouseleave", moreMenuCloseFunction);
+moreMenuOpen.addEventListener("mouseleave", moreMenuCloseFunction);
+
+
+
+const $header = document.querySelector("[data-header]");
+
+const scrollWindow = () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+}
+
+window.addEventListener("scroll", scrollWindow);
